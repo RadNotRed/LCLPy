3a4353ad5e227d493e7a9f83d62aba93075fafcf
Aetopia <41850963+Aetopia@users.noreply.github.com>
2021-10-23 14:54:33 +0530

    Added --clean.

--- build.bat
+++ build.bat
@@ -1,1 +1,1 @@
-py -OO -m PyInstaller main.py -p modules -n "LCLPy" --workpath "%TEMP%" --specpath %TEMP% --noconfirm --onefile --icon NONE
+py -OO -m PyInstaller main.py -p modules -n "LCLPy" --workpath "%TEMP%" --specpath %TEMP% --noconfirm --onefile --icon NONE --clean
