7a7e56040fe61b4d8c0f0c0c483b0c92c455c105
he3als <65787561+he3als@users.noreply.github.com>
2021-10-23 10:01:28 +0100

    Update config.py

--- modules/config.py
+++ modules/config.py
@@ -13,7 +13,8 @@ def ConfigCreate():
                            '1.8 Directory': APPDATA+"\\.minecraft",
                            '1.12 Directory': APPDATA+"\\.minecraft",
                            '1.16 Directory': APPDATA+"\\.minecraft",
-                           '1.17 Directory': APPDATA+"\\.minecraft"
+                           '1.17 Directory': APPDATA+"\\.minecraft",
+                           '1.18 Directory': APPDATA+"\\.minecraft"
                            }
     config['Java'] = {'Arguments': "-Xms3G -Xmx3G -Xmn1G -XX:+DisableAttachMechanism",
                       '1.7 Java': USERPROFILE+"\\.lunarclient\\jre\\zulu16.30.15-ca-fx-jre16.0.1-win_x64\\bin\\javaw.exe",
@@ -21,6 +22,7 @@ def ConfigCreate():
                       '1.12 Java': USERPROFILE+"\\.lunarclient\\jre\\zulu16.30.15-ca-fx-jre16.0.1-win_x64\\bin\\javaw.exe",
                       '1.16 Java': USERPROFILE+"\\.lunarclient\\jre\\zulu16.30.15-ca-fx-jre16.0.1-win_x64\\bin\\javaw.exe",
                       '1.17 Java': USERPROFILE+"\\.lunarclient\\jre\\zulu16.30.15-ca-fx-jre16.0.1-win_x64\\bin\\javaw.exe",
+                      '1.18 Java': USERPROFILE+"\\.lunarclient\\jre\\zulu16.30.15-ca-fx-jre16.0.1-win_x64\\bin\\javaw.exe",
                       }
     config['Optimizations'] = {'Minecraft Launcher Optimizations' : "Off",
                                'LC Cosmetics' : "On"
@@ -54,6 +56,10 @@ def ConfigRead(Version, Value):
     elif Version=="1.17":
         Java_Path=config['Java']["1.17 Java"]
         Directory=config['Minecraft']["1.17 Directory"]
+        Cosmetics=config['Optimizations']['LC Cosmetics']
+    elif Version=="1.18":
+        Java_Path=config['Java']["1.18 Java"]
+        Directory=config['Minecraft']["1.18 Directory"]
         Cosmetics=config['Optimizations']['LC Cosmetics']
     if Value=="Java_Path":    
         return Java_Path
