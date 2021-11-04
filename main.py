ad9de5c6ed4029e50dd471629e5d39e8b43f4ab8
Aetopia <41850963+Aetopia@users.noreply.github.com>
2021-11-05 00:01:41 +0530

    New Debug Argument.

--- main.py
+++ main.py
@@ -10,11 +10,11 @@ import os
 
 # Working Directory
 os.chdir(os.path.dirname(sys.executable))
-     
+      
 #Checks if Options.ini exists or not.
 config.ConfigExist()
 
-# CLI Arguments
+#CLI Arguments
 parser=argparse.ArgumentParser(prog="Lunar Client Lite Python (LCLPy)",
                                description="A debloated, feature rich and CLI based launcher for Lunar Client. Made in Python.",
                                usage="\nLCLPy -v <Version>\nLCLPy -s <Version> <Server IP>")
@@ -24,6 +24,12 @@ command.add_argument('-v','-version',
                      nargs=1,
                      metavar=("<Version>"),
                      help="Version => 1.7/1.8/1.12/1.16/1.17/1.18")
+command.add_argument('-d','-debug',
+                     action="store",
+                     nargs=1,
+                     metavar=("<Version>"),
+                     help="Displays the selected version, JRE path, launch directory, arguments and launch command.")
+
 command.add_argument('-s','-server',
                      action="store",
                      nargs=2,
@@ -36,11 +42,10 @@ args = parser.parse_args()
 
 #Arguments
 if args.v != None:
-    launch.Launch(str(args.v[0])," ")
+    launch.Launch(str(args.v[0]), " ", False)
 elif args.edit == True:
-    subprocess.Popen(["start",
-                      "Options.ini"],
-                     shell=True)
+    subprocess.Popen(["start", "Options.ini"], shell=True)
 elif args.s != None:
-    launch.Launch(str(args.s[0]),
-                  "-server "+args.s[1])
+    launch.Launch(str(args.s[0]), "-server "+args.s[1], False)
+elif args.d != None:
+    launch.Launch(str(args.d[0]), " ", True)
