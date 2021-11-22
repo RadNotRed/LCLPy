7e566f8712473136ef4bae86b1406eceae61a1bd
Aetopia <41850963+Aetopia@users.noreply.github.com>
2021-11-22 11:49:15 +0530

    Update build_nuitka.bat

--- build_nuitka.bat
+++ build_nuitka.bat
@@ -1,4 +1,4 @@
 @echo off
-set Version="1.1.0.0"
-python -OO -m nuitka --onefile --standalone --follow-imports --include-plugin-files=modules\launch.py --include-plugin-files=modules\config.py main.py --output-dir="%TEMP%" -o "LCLPy.exe" --windows-file-description="Lunar Client Lite Python" --windows-product-name="Lunar Client Lite Python" --windows-file-version=%Version% --windows-company-name=" "
+set Version="1.2.0.0"
+py -OO -m nuitka --onefile --standalone --follow-imports --include-plugin-files=modules\launch.py --include-plugin-files=modules\config.py main.py --output-dir="%TEMP%" -o "LCLPy.exe" --windows-file-description="Lunar Client Lite Python" --windows-icon-from-ico=icon.ico --windows-product-name="Lunar Client Lite Python" --windows-file-version=%Version% --windows-company-name=" "
 timeout 5
